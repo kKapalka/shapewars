648a315f06d76c258439dea1af72da62123bf927
kKapalka <k.kapalka@hotmail.com>
2018-12-29 17:29:13 +0100

    display all relevant shape data

--- front-end/src/app/admin/shape-panel/shape-panel.component.ts
+++ front-end/src/app/admin/shape-panel/shape-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ShapeService} from "../../services/shape.service";
+import {SkillsService} from "../../services/skills.service";
 
 @Component({
   selector: 'app-shape-panel',
@@ -9,12 +10,18 @@ import {ShapeService} from "../../services/shape.service";
 export class ShapePanelComponent implements OnInit {
 
   shapes$;
-  constructor(private service: ShapeService) {
+  skills;
+  constructor(private service: ShapeService, private skillService:SkillsService) {
     this.service.getAllShapes().subscribe(res=>{
       this.shapes$=res;
+    });
+    this.skillService.getAllSkills().subscribe(res=>{
+      this.skills=res;
     })
   }
-
+  filterSkillsWithIds(ids:number[]){
+    return this.skills.filter(skill=>ids.includes(skill.id));
+  }
   ngOnInit() {
   }
 
