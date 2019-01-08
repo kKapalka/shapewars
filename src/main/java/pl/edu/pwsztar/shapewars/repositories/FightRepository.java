632fbf48b640139f58fd29596c70a9b85d7150a2
Kamil Kapalka <kamil.kapalka@comarch.pl>
2019-01-08 14:17:57 +0100

    metody pobierające walki gracza

--- src/main/java/pl/edu/pwsztar/shapewars/repositories/FightRepository.java
+++ src/main/java/pl/edu/pwsztar/shapewars/repositories/FightRepository.java
@@ -1,10 +1,25 @@
 package pl.edu.pwsztar.shapewars.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pl.edu.pwsztar.shapewars.entities.Fight;
+import pl.edu.pwsztar.shapewars.entities.User;
 
 @Repository
 public interface FightRepository extends JpaRepository<Fight,Long> {
+
+    @Query("select f from Fight f where (f.playerOne=?1 and f.fightStatus like 'VICTORY_PLAYER_ONE')" +
+           " or (f.playerTwo=?1 and f.fightStatus like 'VICTORY_PLAYER_TWO')")
+    List<Fight> findAllWonByUser(User user);
+
+    @Query("select f from Fight f where (f.playerTwo=?1 and f.fightStatus like 'VICTORY_PLAYER_ONE')" +
+           " or (f.playerOne=?1 and f.fightStatus like 'VICTORY_PLAYER_TWO')")
+    List<Fight> findAllLostByUser(User user);
+
+    @Query("select f from Fight f where (f.playerOne=?1 or f.playerOne=?2) and f.fightStatus like 'ABANDONED'")
+    List<Fight> findAllAbandonedByUser(User user);
+
 }
