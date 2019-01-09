82e7123c3db3fc2d9b66a2cd166e6fce174a8c61
kKapalka <k.kapalka@hotmail.com>
2019-01-09 20:17:22 +0100

    display damage dealt on color panel

--- front-end/src/app/admin/colormap-panel/colormap-panel.component.ts
+++ front-end/src/app/admin/colormap-panel/colormap-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ColormapService} from "../../services/colormap.service";
+import ColorMap from "../../dtos/colorMap";
 
 @Component({
   selector: 'app-colormap-panel',
@@ -8,13 +9,22 @@ import {ColormapService} from "../../services/colormap.service";
 })
 export class ColormapPanelComponent implements OnInit {
 
-  colors;
+  colors:ColorMap[]=[];
   constructor(private service: ColormapService) {
     this.service.getAllColors().subscribe(res=>{
       this.colors=res;
     })
   }
-
+  composeEffect(color:ColorMap){
+    if(color.colorDamageDtoList.length!==0) {
+      return "Deals " + color.colorDamageDtoList.sort((a,b)=>{
+        return a.id-b.id;
+      }).map
+      (color => color.damagePercentage + "% to " + color.enemyColorName);
+    } else{
+      return "Needs to be configured";
+    }
+  }
   ngOnInit() {
   }
 
